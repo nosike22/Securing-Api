9f6f550eec6ffc333156427bf901e09a3c483283
nosike22 <frank.nosike@gmail.com>
2022-05-23 10:53:22 +0100

    Securing an Api

---
+++ Models/AddRoleModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SecureApi.Models
+{
+    public class AddRoleModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
---
+++ Services/IUserService.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using SecureApi.Models;
+
+namespace SecureApi.Services
+{
+    public interface IUserService
+    {
+        Task<string> RegisterAsync(RegisterModel model);
+        Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+    }
+}
